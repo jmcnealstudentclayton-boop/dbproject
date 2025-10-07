b3391d6fa307d69d1e54484a16ebb510ed32585d
JusticeMcNealLLC <justin.mcnealllc@gmail.com>
2025-10-07 16:29:31 -0400

    w

--- js/pages/forms/f_googlesheet/config.js
+++ js/pages/forms/f_googlesheet/config.js
@@ -3,7 +3,7 @@ export const WEBHOOK_BUILD = 'https://hook.us2.make.com/h10r84lw72sl48qdw07cq8m7
 export const REVIEW_WEBHOOK = 'https://hook.us2.make.com/ic641vk7icbq1x4u317qef33ouqakqm5';
 
 // Your product catalog (same-origin recommended)
-export const PRODUCTS_JSON_URL = '/products/products.json';
+export const PRODUCTS_JSON_URL = 'https://jmcnealstudentclayton-boop.github.io/<repo>/products/products.json';
 
 // Cache TTL for catalog (ms)
 export const CACHE_TTL_MS = 5 * 60 * 1000;
